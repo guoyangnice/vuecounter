89ace53d65187ac1072528a6008c79bd540b9384
guoyangnice <yangjienice@126.com>
2019-09-01 00:47:07 +0800

    增加counter代码

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>click {{count}} times,count is {{oddOrEven}}</p>
+      <button @click="inc">+</button>
+      <button @click="dec">-</button>
+  </div>
+</template>
+
+<script>
+  export default {
+		data() {
+			return {
+				count: 0
+			}
+		},
+		methods: {
+			//增加
+			inc() {
+				const count = this.count
+				this.count = count + 1
+			},
+			//减少
+			dec() {
+				const count = this.count
+				this.count = count - 1
+			}
+		},
+	}
+</script>
+
+<style>
+
+</style>
